5e7b414d9114407dc5a565e7e93106ab9ee36e69
d3ataviz <d3ataviz@gmail.com>
2019-12-18 21:18:49 +0100

    intermediate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Chart1Component } from './charts/chart1/chart1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chart1Component
   ],
   imports: [
     BrowserModule,

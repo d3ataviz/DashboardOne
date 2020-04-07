8bdd64f14bd366fcfa371a814cd46c40076e963e
d3ataviz <d3ataviz@gmail.com>
2020-04-08 00:03:48 +0200

    svg - before ngfor

--- src/app/charts/chart1/chart1.component.ts
+++ src/app/charts/chart1/chart1.component.ts
@@ -7,43 +7,12 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class Chart1Component implements OnInit {
 
-  data = [125, 100, 50, 75, 200, 300, 100];
-  xlabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  data = [125, 100, 50, 75, 200];
 
-  rectWidth = 80;
-  max = 250;
-  dimensions: DOMRect;
-  outerPadding = 20;
-  padding = 0;
-  bandwidth = 0;
-  bandwidthCoef = 0.8; // 80% = 0.8;
-  left = 10; right = 20; bottom = 16; top = 15;
-  innerWidth: number;
-  innerHeight: number;
-/*   margin = {
-    left: 10,
-    right: 20,
-    bottom:30,
-    top: 15
-  }; */
-
-  // width = width svg / number of rectangles
-  // width = svg.width / data.length
-
-  constructor(private element: ElementRef) {
+  constructor() {
    }
 
   ngOnInit() {
-    const svg = this.element.nativeElement.getElementsByTagName('svg')[0];
-    this.dimensions = svg.getBoundingClientRect();
-    this.innerWidth = this.dimensions.width - this.left - this.right;
-    this.innerHeight = this.dimensions.height - this.top - this.bottom;
-
-    this.rectWidth = (this.innerWidth - 2 * this.outerPadding) / this.data.length;
-    this.bandwidth = this.bandwidthCoef * this.rectWidth;
-    this.padding = (1 - this.bandwidthCoef) * this.rectWidth;
-    console.log(svg, svg.getBoundingClientRect());
-    this.max = 1.3 * Math.max(...this.data); // 1.3 = 130%
   }
 
 }
